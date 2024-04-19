ddb7f78ad597027b42f75782b46c4905ba3f8ffe
Tony Skorik <just.skorik@gmail.com>
2024-04-19 15:12:52 +0400

    Typo fixes;

---
+++ tests/Aer.Memcached.Tests/Infrastructure/Logging/TestNodeHealthCheckerLoggerWrapper.cs
@@ -0,0 +1,10 @@
+﻿using Aer.Memcached.Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace Aer.Memcached.Tests.Infrastructure.Logging;
+
+internal class TestNodeHealthCheckerLoggerWrapper : TestLoggerWrapperBase, ILogger<NodeHealthChecker<Pod>>
+{
+	public TestNodeHealthCheckerLoggerWrapper(ILogger logger) : base(logger)
+	{ }
+}
