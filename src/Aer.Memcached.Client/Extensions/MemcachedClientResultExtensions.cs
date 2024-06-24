b364fd098e3e1d84cefe9483d8f4fec1f109b3bc
Tony Skorik <just.skorik@gmail.com>
2024-06-24 17:34:54 +0400

    Add client result error logging extension methods

--- src/Aer.Memcached.Client/Extensions/MemcachedClientResultExtensions.cs
+++ src/Aer.Memcached.Client/Extensions/MemcachedClientResultExtensions.cs
@@ -1,12 +1,87 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using Aer.Memcached.Client.Models;
+using Microsoft.Extensions.Logging;
 
 namespace Aer.Memcached.Client.Extensions;
 
-internal static class MemcachedClientResultExtensions
+/// <summary>
+/// Contains extension methods for value and generic client results. 
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "MemberCanBeInternal")]
+public static class MemcachedClientResultExtensions
 {
-    public static MemcachedClientResult WithSyncSuccess(this MemcachedClientResult result, bool syncSuccess)
+    private const string DefaultErrorMessage = "UNKNOWN - see memcached command execution logs : search for substring 'Error occured during command'";
+    
+    internal static MemcachedClientResult WithSyncSuccess(this MemcachedClientResult result, bool syncSuccess)
     {
         result.SyncSuccess = syncSuccess;
         return result;
+    }
+
+    /// <summary>
+    /// Checks client method result and logs error if client method call returned non-successful result. 
+    /// </summary>
+    /// <param name="target">The client result to check.</param>
+    /// <param name="logger">The logger to log error to.</param>
+    /// <param name="operationName">
+    /// The operation name that called client method. Do not assign - assigned automatically.
+    /// </param>
+    public static void LogErrorIfAny(
+        this MemcachedClientResult target,
+        ILogger logger,
+        [CallerMemberName] string operationName = null)
+    {
+        if (target.Success)
+        {
+            return;
+        }
+
+        logger.LogError(
+            "Error happened during memcached {Operation} operation. Error details : {ErrorDetails}",
+            operationName,
+            target.ErrorMessage ?? DefaultErrorMessage
+        );
+    }
+
+    /// <summary>
+    /// Checks client method result and logs error if client method call returned non-successful result. 
+    /// </summary>
+    /// <param name="target">The client result to check.</param>
+    /// <param name="logger">The logger to log error to.</param>
+    /// <param name="cacheKeysCount">The optional number of keys requested.</param>
+    /// <param name="operationName">
+    /// The operation name that called client method. Do not assign - assigned automatically.
+    /// </param>
+    public static void LogErrorIfAny<T>(
+        this MemcachedClientValueResult<T> target,
+        ILogger logger,
+        int? cacheKeysCount,
+        [CallerMemberName] string operationName = null)
+    {
+        if (target.Success)
+        {
+            return;
+        }
+
+        if (cacheKeysCount is { } keysCount)
+        {
+            logger.LogError(
+                "Error happened during memcached {Operation} operation with cache keys count : {CacheKeysCount}. Error details : {ErrorDetails}",
+                operationName,
+                keysCount,
+                target.ErrorMessage ?? DefaultErrorMessage
+            );
+        }
+        else
+        { 
+            // means cacheKeysCount not specified
+            logger.LogError(
+                "Error happened during memcached {Operation} operation. Error details : {ErrorDetails}",
+                operationName,
+                target.ErrorMessage ?? DefaultErrorMessage
+            );
+        }
     }
 }
